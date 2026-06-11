6bd4774d322ba5c9886d15b30a6a02d39cdbcf48
Archishmaan74 <archishmaan.upa4@gmail.com>
2026-06-11 14:26:01 +0530

    updated habit lists with MUI comps

--- habit-tracker/src/components/HabitList.tsx
+++ habit-tracker/src/components/HabitList.tsx
@@ -1,19 +1,40 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../app/store";
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import { CheckCircle, Delete } from "@mui/icons-material";
 
 function HabitList() {
   const habitsList = useSelector((state: RootState) => state.habits.habits);
+  const today = new Date().toISOString().split("T")[0];
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
-      {habitsList.map((habits) => (
-        <Paper key={habits.id} elevation={2} sx={{ p: 2 }}>
+      {habitsList.map((habit) => (
+        <Paper key={habit.id} elevation={2} sx={{ p: 2 }}>
           <Grid container sx={{ alignItems: "center" }}>
-            <Grid>
-              <Typography variant="h6">{habits.name}</Typography>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <Typography variant="h6">{habit.name}</Typography>
               <Typography variant="body2" sx={{ textTransform: "capitalize" }}>
-                {habits.frequency}
+                {habit.frequency}
               </Typography>
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6 }}>
+              <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
+                <Button
+                  variant="outlined"
+                  color={
+                    habit.completedDates.includes(today) ? "success" : "primary"
+                  }
+                  startIcon={<CheckCircle />}
+                >
+                  {habit.completedDates.includes(today)
+                    ? "Completed"
+                    : "Mark Complete"}
+                </Button>
+                <Button variant="outlined" color="error" startIcon={<Delete />}>
+                  Remove
+                </Button>
+              </Box>
             </Grid>
           </Grid>
         </Paper>
