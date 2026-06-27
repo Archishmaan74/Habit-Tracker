f514fa955e06e2e4c8225b99914cf74207f55499
Archishmaan74 <archishmaan.upa4@gmail.com>
2026-06-27 15:18:47 +0530

    implemented test cases for add habit form

--- habit-tracker/src/components/AddHabitForm.test.tsx
+++ habit-tracker/src/components/AddHabitForm.test.tsx
@@ -2,7 +2,7 @@ import { Provider } from "react-redux";
 import { describe, expect, test } from "vitest";
 import store from "../app/store";
 import AddHabitForm from "./AddHabitForm";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 const renderWithProvider = (ui: React.ReactElement) =>
   render(<Provider store={store}>{ui}</Provider>);
@@ -16,5 +16,22 @@ describe("Add Habit Form", () => {
     expect(
       screen.getByRole("combobox", { name: /Frequency/i }),
     ).toBeInTheDocument();
+  });
+
+  test("should show menu items of click of frequency", () => {
+    renderWithProvider(<AddHabitForm />);
+    const combobox = screen.getByRole("combobox", { name: /Frequency/i });
+    fireEvent.mouseDown(combobox);
+    expect(screen.getByRole("option", { name: "Daily" })).toBeInTheDocument();
+    expect(screen.getByRole("option", { name: "Weekly" })).toBeInTheDocument();
+  });
+
+  test("should show selected frequency after selection in the frequency field", () => {
+    renderWithProvider(<AddHabitForm />);
+    const combobox = screen.getByRole("combobox", { name: /Frequency/i });
+    fireEvent.mouseDown(combobox);
+    const clikedWeekly = screen.getByRole("option", { name: /Weekly/i });
+    fireEvent.click(clikedWeekly);
+    expect(combobox).toHaveTextContent("Weekly");
   });
 });
